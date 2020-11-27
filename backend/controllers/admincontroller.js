aa9800881c73dc5fff8ac99dc84367795422103c
cdgmx <xcdgm@users.noreply.github.com>
2020-11-27 19:47:30 +0800

    added admincontroller
    admin  controller can query its own info and scanned data for now

--- backend/controllers/admincontroller.js
+++ backend/controllers/admincontroller.js
@@ -1,20 +1,22 @@
+const express = require('express')
+const dboperations = require ('../database/dboperations')
+let router = express.Router()
+//future purposes, not being used
 
-
-const express = require("express");
-const router = express.Router();
 var client = "users"
 var client_id = "qwerqwe"
 // var qrkey = "135"
 var QRCode = require('qrcode');
-const dboperations = require("../database/dboperations");
 
+router //for gettin and updating
+ 
 router
-    .route('/info') //geting the info of admin like name and address to display
+    .route('/info')
     .get(async (req,res) =>{
         try{
-            //needed qrkey 
-            let qrkey = req.body.qrkey
-            let data = await dboperations.getClientInfo("users",'qrkey',qrkey,'client_id','admins')
+            //needed client_id of admin
+            let client_id = req.body.client_id
+            let data = await dboperations.getClientInfo("admins",'client_id',client_id)
             if(data){
                 res.send(data)
             }
@@ -25,35 +27,28 @@ router
         }
         catch(error) {
             console.log(error)
-            res.status(401).send({message:error});
+            res.status(401).send(error);
         }
     })
 router
-    .route('/history')
+    .route('/scanned')
     .get(async (req,res) =>{
         try{
-             //needed client_id of user
+            //needed client_id, it will select all data from  scanned table where admin_id = client_id
+            //it will only get its own scanned data
             let client_id = req.body.client_id
-            let data = await dboperations.getScanned(client_id,'user_id','admin_id','admins')
+            let data = await dboperations.getScanned(client_id,'admin_id','user_id','users')
             if(data){
                 console.log("history sent")
                 res.send(data)
             }
             else{
-                throw "error in getClientInfo"
+                throw "error in getScanned"
             }
         }
         catch(error) {
             console.log(error)
-            res.status(401).send({message:error});
+            res.status(401).send(error);
         }
     })
-
 module.exports = router
-
-
-
-
-
-
-module.exports = router;
