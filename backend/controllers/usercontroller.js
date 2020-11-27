30fbfccc1f336b9be1c49997096b880b49225fad
cdgmx <xcdgm@users.noreply.github.com>
2020-11-27 19:30:23 +0800

    Update usercontroller.js

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -34,6 +34,7 @@ router //for gettin and updating
         .put((req,res) =>{
             try{
                 //updating the qrkey to resetqr
+                
                 //needed the new qrkey
                 let qrkey = req.body.qrkey
                 let client_id = req.body.client_id
